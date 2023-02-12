1830a0187d50128e6a1ec37ca9f36746fdf8dceb
Roman <vetdzen@gmail.com>
2023-02-12 16:50:33 +0300

    определение четности числа

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+
+string answer = Console.ReadLine();
+int a = Convert.ToInt32(answer); 
+ 
+if (a == (a % 2)) 
+ {
+     Console.WriteLine("нет!");
+ }
+ else Console.WriteLine("да!");
