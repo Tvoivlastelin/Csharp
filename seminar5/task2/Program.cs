6316967f1623c52c505f4934920c6d76b53cca19
Roman <vetdzen@gmail.com>
2023-02-25 16:18:30 +0300

    5 семинар домашнее задание 2

---
+++ seminar5/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Задайте одномерный массив, 
+//  заполненный случайными числами. 
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int [] GetRandomArray(int length, int leftRange,int rightRange)
+{
+    int [] array = new int [length];
+    for(int i=0; i<length;i++)
+    {
+        array[i]=Random.Shared.Next(leftRange,rightRange);
+    
+    }
+    return array;
+}
+int Sum =0;
+int SumNumberNechIndexArray(int [] array, int Sum)
+{
+    for(int i=0; i<array.Length;i++)
+    {
+       if(i%2!=0)
+       {
+        Sum = Sum + array[i];
+       }
+    }
+    return Sum;
+}
+
+int[] myArray = GetRandomArray(6,-9,10);
+int summa = SumNumberNechIndexArray(myArray, Sum);
+Console.WriteLine($"[{string.Join( ", " , myArray)}] " + summa);
