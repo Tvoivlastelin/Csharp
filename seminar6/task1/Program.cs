e110c29e6ecd4b3c24766904463f7cd71e5da776
Roman <vetdzen@gmail.com>
2023-03-02 21:21:42 +0300

    6 семинар домашнее задание 1

---
+++ seminar6/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите колличество элементов в массиве");
+int element = Convert.ToInt32(Console.ReadLine());
+    int [] myArray = new int[element];
+     int count = 0;
+ 
+      for(int i=0;i<myArray.Length;i++)
+      {
+         Console.WriteLine($"Введите число масcива с индексом {i}");
+         myArray[i]=Convert.ToInt32(Console.ReadLine());
+         if(myArray[i]>0)
+         count++;
+      }
+      Console.WriteLine($"[{string.Join(", ", myArray)}] -> {count}");
