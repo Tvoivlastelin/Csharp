f7b0f9cc13d084b0f3bc503009e089880dfd59c8
Roman <vetdzen@gmail.com>
2023-03-07 18:59:05 +0300

    7 семинар домашнее задание 2 доделанное

--- seminar7/task2/Program.cs
+++ seminar7/task2/Program.cs
@@ -45,13 +45,13 @@ for (int i = 0; i < myMatrix.GetLength(0); i++)
 {
     for (int j = 0; j < myMatrix.GetLength(1); j++)
     {
-        if (pi < m || pj < n)
+        if ( pi >= m || pj >= n)
         {
-            val = true;
+            val = false;
         }
         else
         {
-            val = !val;
+            val = true; 
         }
     }
 }
