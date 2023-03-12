910ff0d6c7c2eb53d152bf3cfa0f25dbb300e78e
Roman <vetdzen@gmail.com>
2023-03-12 11:55:05 +0300

    9 семинар задание 1

---
+++ seminar9/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = 8;
+void NumbersOtNToOne(int n)
+{
+    if (n == 0) return;
+    else
+    {
+
+        NumbersOtNToOne(n - 1);
+        Console.Write(number - n + 1 + ", ");
+    }
+}
+NumbersOtNToOne(number);
